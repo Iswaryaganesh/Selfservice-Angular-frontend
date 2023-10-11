559382fb9c7d293caca651eb2234b51ebc152523
Velu-1911 <pethachi.pr@prodapt.com>
2023-10-11 17:12:33 +0530

    Final work done october 11  05:23

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,2 +1,17 @@
 export class Router {
+    routerID:number;
 
+    accountNumber:number;
+
+    serialNumber:String;
+
+    model:String;
+
+    firmwareVersion:String;
+
+    ipv4:String;
+
+    ssid:String;
+
+    password:String;
+}
